45e385ff65b64a6cae333f38587756d77def1a9d
kafendt <c.kahlefendt@gmail.com>
2018-04-01 02:13:38 +0200

    successfully detect definiton or declaration

--- rplugin/python3/cpp-tools.py
+++ rplugin/python3/cpp-tools.py
@@ -11,16 +11,21 @@ class CPPTools(object):
         # get current line content
         line_content = self.nvim.current.line
 
-        # check for a declaration
-        declaration_groups = re.findall("(\w+([:\w<>,\s*&]+)?)\s([\w:]+)\(((?:[\w\s:*&<>]+,?\s?)*)\);", line_content)
-        if len(declaration_groups) > 0:
-            self.nvim.out_write("Found Declaration!\n")
+        # check if there is a function declaration/definiton in the current line
+        function_groups = re.findall("(\w+([:\w<>,\s*&]+)?)\s([\w:]+)\(((?:[\w\s:*&<>]+,?\s?)*)\)", line_content)
+        if len(function_groups) > 0:
+            # check for a declaration (line ends with a ;)
+            if line_content.endswith(";"):
+                self.nvim.out_write("Found Declaration!\n")
+                return
+            # check for a definition (this line ends with or next line starts with a {)
+            if line_content.endswith("{"):
+                self.nvim.out_write("Found Definiton!\n")
+                return
+            next_line_content = self.nvim.funcs.getline(self.nvim.funcs.line('.') + 1)
+            if "{" in next_line_content:
+                self.nvim.out_write("Found Definiton!\n")
+                return
+        else:
+            self.nvim.out_write("Nothing found!\n")
             return
-
-        # check for a definition
-        definition_groups = re.findall("(\w+([:\w<>,\s*&]+)?)\s([\w:]+)\(((?:[\w\s:*&<>]+,?\s?)*)\)\n*{", line_content)
-        if len(definition_groups) > 0:
-            self.nvim.out_write("Found Definiton!\n")
-            return
-
-        self.nvim.out_write("Nothing found!\n")
